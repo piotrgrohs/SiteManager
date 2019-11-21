251e274d760ed5879fedfe349ec4efbcae82f4b1
Piotr Grohs <piotrgrohs@gmail.com>
2019-11-21 23:00:33 +0100

    add simple pages

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,9 @@
 
 from django.conf.urls import url
 
-from .views import homePageView
+from .views import HomePageView, AboutPageView
 
 urlpatterns = [
-    url('', homePageView, name='home')
+    url(r'^about/', AboutPageView.as_view(), name='about'),
+    url(r'', HomePageView.as_view(), name='home')
 ]
--- website/views.py
+++ website/views.py
@@ -3,7 +3,11 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
-def homePageView(request):
-    return HttpResponse('Hello, World!')
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
