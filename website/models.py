9605f2c27d371e55a979a8d3ab0190edd67f8788
Piotr Grohs <piotrgrohs@gmail.com>
2019-12-01 14:27:59 +0100

    add unique folder for file upload

--- website/models.py
+++ website/models.py
@@ -3,14 +3,22 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.utils import timezone
+import uuid
+import os
 
-# Create your models here.
-
+def get_file_path(instance, filename):
+    folder = "%s/%s" % (uuid.uuid4(),filename)
+    return os.path.join('website/templates/uploads/',folder)
 
 class Model(models.Model):
     title = models.CharField(max_length=200)
     created_date = models.DateTimeField(default=timezone.now)
-    file = models.FileField(upload_to='website/templates/uploads/', default='upload file' )
+    file = models.FileField(upload_to=get_file_path,
+                        null=True,
+                        blank=True,
+                        verbose_name='Website')
+    def filepath(self):
+        return str(self.file).replace('website/templates/','')
 
     def __str__(self):
         return self.title
